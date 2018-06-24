68d51c895c1f9165ebda74277c801131e7c0a9ab
Matej Minarik <mato.minarik@azet.sk>
2018-06-24 18:49:02 +0200

    Bump version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TicTacToe.MixProject do
   def project do
     [
       app: :tic_tac_toe_math3v,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
